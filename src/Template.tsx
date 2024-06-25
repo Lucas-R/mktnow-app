2b39e4ac868cd482dc3bd921d9d74117b8e2777d
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2024-06-25 19:45:02 -0300

    feat: new menu

---
+++ src/Template.tsx
@@ -0,0 +1,9 @@
+import { Router } from "./Router"
+
+export const Template = () => {
+    return(
+        <div className="min-w-full min-h-screen bg-zinc-50 font-sans dark:bg-zinc-950">
+            <Router />
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Router } from './Router'
+import { Template } from './Template'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <Template />
   </React.StrictMode>,
 )
